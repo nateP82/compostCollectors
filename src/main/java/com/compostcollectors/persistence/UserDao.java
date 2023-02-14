efa01f52df48c24071fce008985cc4a36ace1c14
nateP82 <npeck@madisoncollege.edu>
2023-02-14 11:34:20 -0600

    creating UserDao with one method, getAllUsers

---
+++ src/main/java/com/compostcollectors/persistence/UserDao.java
@@ -0,0 +1,31 @@
+package com.compostcollectors.persistence;
+
+import com.compostcollectors.entity.User;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class UserDao {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+    SessionFactory sessionFactory = SessionFactoryProvider.getSessionFactory();
+
+    /** method getAllUsers
+     * Method that selects all users from the user database.
+     * @return executeQuery(sql) results from the database query
+     */
+    public List<User> getAllUsers() {
+        Session session = sessionFactory.openSession();
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        List<User> users = session.createQuery(query).getResultList();
+        session.close();
+        return users;
+    }
+}
