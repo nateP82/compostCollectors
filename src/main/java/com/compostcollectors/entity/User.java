7863f0fa595d299d1e175d37f4d570502ad99d5e
nateP82 <npeck@madisoncollege.edu>
2023-02-21 11:16:53 -0600

    adding getters and setters for id in User javabean, updating UserDaoTest with tests

--- src/main/java/com/compostcollectors/entity/User.java
+++ src/main/java/com/compostcollectors/entity/User.java
@@ -57,6 +57,24 @@ public class User {
     }
 
     /**
+     * Gets id.
+     *
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
      * Gets first name.
      *
      * @return the first name
--- src/test/java/com/compostcollectors/persistence/UserDaoTest.java
+++ src/test/java/com/compostcollectors/persistence/UserDaoTest.java
@@ -1,13 +1,17 @@
 package com.compostcollectors.persistence;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.compostcollectors.entity.User;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserDaoTest {
+    private final Logger logger = LogManager.getLogger(this.getClass());
     UserDao dao;
 
     /**
@@ -16,6 +20,7 @@ class UserDaoTest {
      */
     @BeforeEach
     void setUp() {
+        logger.info("Running setup before each test");
         dao = new UserDao();
         com.compostcollectors.test.util.Database database = com.compostcollectors.test.util.Database.getInstance();
         database.runSQL("cleandb.sql");
@@ -27,7 +32,84 @@ class UserDaoTest {
      */
     @Test
     void getAllUsersSuccess() {
+        logger.info("Running getAllUsers Test");
         List<User> users = dao.getAllUsers();
         assertEquals(6, users.size());
+    }
+    /**
+     * test method getUsersByLastNameSuccess
+     * test that checks for the expected number of users with a given search value is returned.
+     */
+    @Test
+    void getUsersByLastNameSuccess() {
+        logger.info("Running getUsersByLastName Test");
+        List<User> users = dao.getUsersByLastName("c");
+        assertEquals(3, users.size());
+    }
+    /**
+     * test that verifies the correct user is returned when given a certain id
+     */
+    @Test
+    void getUserByIdSuccess() {
+        logger.info("Running getUserById test");
+        User retrievedUser = dao.getUserById(3);
+        assertNotNull(retrievedUser);
+        assertEquals("Barney", retrievedUser.getFirstName());
+    }
+    /**
+     * Verify successful insert of a user
+     */
+    @Test
+    void insertSuccess() {
+        logger.info("Running insertUser test");
+        User newUser = new User("Fred", "Flintstone", "fflintstone", "fflintstone@mailinator.com", "supersecret7", "1029 Street Address", 40);
+        int id = dao.insert(newUser);
+        assertNotEquals(0,id);
+        User insertedUser = dao.getUserById(id);
+        assertEquals("Fred", insertedUser.getFirstName());
+        // Could continue comparing all values, but
+        // it may make sense to use .equals()
+        // TODO review .equals recommendations http://docs.jboss.org/hibernate/orm/5.2/userguide/html_single/Hibernate_User_Guide.html#mapping-model-pojo-equalshashcode
+    }
+    /**
+     * Verify successful delete of user
+     */
+    @Test
+    void deleteSuccess() {
+        logger.info("Running deleteUser test");
+        dao.delete(dao.getUserById(3));
+        assertNull(dao.getUserById(3));
+    }
+    /**
+     * Verifies that a user can be successfully updated
+     */
+    @Test
+    void updateSuccess() {
+        logger.info("Running updateUser test");
+        String newLastName = "Davis";
+        User userToUpdate = dao.getUserById(3);
+        userToUpdate.setLastName(newLastName);
+        dao.saveOrUpdate(userToUpdate);
+        User retreivedUser = dao.getUserById(3);
+        assertEquals(newLastName, retreivedUser.getLastName());
+    }
+    /**
+     * Verify successful get by property (equal match)
+     */
+    @Test
+    void getByPropertyEqualSuccess() {
+        logger.info("Running getByPropertyEqual test");
+        List<User> users = dao.getByPropertyEqual("lastName", "Curry");
+        assertEquals(1, users.size());
+        assertEquals(3, users.get(0).getId());
+    }
+    /**
+     * Verify successful get by property (like match)
+     */
+    @Test
+    void getByPropertyLikeSuccess() {
+        logger.info("Running getByPropertyLike test");
+        List<User> users = dao.getByPropertyLike("lastName", "c");
+        assertEquals(3, users.size());
     }
 }
