1e5458aceab73d8f9b6500bd0fdb8b5960b75cbf
nateP82 <npeck@madisoncollege.edu>
2023-05-03 11:57:01 -0500

    adding javadoc and logger to compostingAPIDao test class.

--- src/test/java/com/compostcollectors/persistence/CompostingAPIDaoTest.java
+++ src/test/java/com/compostcollectors/persistence/CompostingAPIDaoTest.java
@@ -1,13 +1,21 @@
 package com.compostcollectors.persistence;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
+
 import javax.ws.rs.client.*;
 import javax.ws.rs.core.MediaType;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * CompostingAPIDaoTest class
+ * This class tests the return data from the composting API
+ */
 public class CompostingAPIDaoTest {
-
+    private final Logger logger = LogManager.getLogger(this.getClass());
     @Test
     public void testCompostingAPIJSON() throws Exception {
+        logger.info("Running the JSON api access point test");
         Client client = ClientBuilder.newClient();
         WebTarget target =
                 client.target("http://compostingapi-env.eba-x3jcxyuh.us-east-2.elasticbeanstalk.com/composting/servicesV1/json");
